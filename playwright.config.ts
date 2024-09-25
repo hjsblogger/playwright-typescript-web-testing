6f0a9870d5860e408aae44b1ab8116050d83826c
Himanshu Jagdish Sheth <himanshu.sheth@gmail.com>
2024-09-25 20:26:23 +0530

    [Parallel Execution] Modifications in package and config files

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,24 +7,24 @@ const config: PlaywrightTestConfig = {
   timeout: 300000,
   workers: 4,
   use: {},
-  projects: [
-    // -- LambdaTest Config --
-    // name in the format: browserName:browserVersion:platform@lambdatest
-    // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-    // Use additional configuration options provided by Playwright if required: https://playwright.dev/docs/api/class-testconfig
-    {
-      name: "Galaxy S21 5G:12:android@lambdatest",
-      use: {
-        viewport: { width: 360, height: 800 },
-      },
-    },
-    {
-      name: "Galaxy S23 Ultra 5G:14:android@lambdatest",
-      use: {
-        viewport: { width: 412, height: 915 },
-      },
-    },
-  ],
+  // projects: [
+  //   // -- LambdaTest Config --
+  //   // name in the format: browserName:browserVersion:platform@lambdatest
+  //   // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+  //   // Use additional configuration options provided by Playwright if required: https://playwright.dev/docs/api/class-testconfig
+  //   {
+  //     name: "Galaxy S21 5G:12:android@lambdatest",
+  //     use: {
+  //       viewport: { width: 360, height: 800 },
+  //     },
+  //   },
+  //   {
+  //     name: "Galaxy S23 Ultra 5G:14:android@lambdatest",
+  //     use: {
+  //       viewport: { width: 412, height: 915 },
+  //     },
+  //   },
+  // ],
 };
 
 export default config;
